0f5d122e831c212f3d3f475d10ba88d4019f08f6
JGadd74 <jon-k-g94@hotmail.com>
2023-05-31 16:32:54 -0400

    rework of move/archive. Other minor changes.

--- Porgan.py
+++ Porgan.py
@@ -1,16 +1,13 @@
 import argparse
-import os,fnmatch
-from pathlib import Path
+import os
 import zipfile
 import shutil
 import re
 
-
-#TODO: Either remove misc or change unknown folder to 'unknown instead of misc
 Extensions_Dictionary = {
     'images': ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'tiff', 'psd', 'raw', 'heif', 'indd', 'ai', 'eps', 'ps', 'webp'],
     'audio': ['aac', 'aa', 'dvf', 'm4a', 'm4b', 'm4p', 'mp3', 'msv', 'ogg', 'oga', 'raw', 'vox', 'wav', 'wma'],
-    'video': ['3g2', '3gp', 'avi', 'flv', 'h264', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv'],
+    'videos': ['3g2', '3gp', 'avi', 'flv', 'h264', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv'],
     'documents': ['doc', 'docx', 'odt', 'pdf', 'rtf', 'tex', 'txt', 'wks', 'wps', 'wpd', 'csv', 'dat', 'pps', 'ppt', 'pptx', 'ods', 'xls', 'xlsx'],
     'executables': ['apk', 'bat', 'bin', 'cgi', 'pl', 'com', 'exe', 'gadget', 'jar', 'wsf'],
     'archives': ['7z', 'arj', 'pkg', 'rar', 'z', 'zip'],
@@ -25,10 +22,10 @@ Extensions_Dictionary = {
     'models': ['3ds', '3mf', 'blend', 'fbx', 'gltf', 'obj', 'stl', 'dae', 'dxf', 'lwo', 'lws', 'lxo', 'ma', 'max', 'mb', 'mesh', 'mesh.xml', 'obj', 'ply', 'skp', 'wrl', 'x', 'x3d', 'x3db', 'x3dv', 'xgl', 'zgl']
 }
 
-#TODO unbork this
+#set to folder to be organized
 Target_Directory = './Files/Downloads'
 
-# absolute paths of target files for run-anywhere functionality
+#absolute paths of target files for run-anywhere functionality
 def get_absolute_file_paths(folder):
 
     files = []
@@ -38,25 +35,17 @@ def get_absolute_file_paths(folder):
             files.append(os.path.abspath(os.path.join(folder, f)))
     return files  
 
-# list of files to ignore
-def get_ignore_list():
-    #TODO make it possible to add more files to ignore list
-    #TODO switch to Extensions_Dictionary from Extensions_categories
-    
-    ignore_list = []
-    for e in Extensions_categories:
-        for key, val in e.items():
-            ignore_list.append(f'{Target_Directory}/{key}.zip')
-    return ignore_list
-
-# All files in Target_Directory, unsorted, with some files ignored
-Raw_File_List = [f for f in get_absolute_file_paths(Target_Directory) if f not in get_ignore_list()]
-
+#get all possible zip files that can be made
 def get_app_made_zips():
     zips = []
-    filenames = Extensions_categories.keys()
+    filenames = list(Extensions_Dictionary.keys())
     for f in filenames:
-        print(f)
+        zips.append(f + '.zip')
+    return zips
+
+#get all files that are not created by this app
+def get_raw_file_list():
+    return [f for f in get_absolute_file_paths(Target_Directory) if f not in get_app_made_zips()]
 
 #get args
 parser = argparse.ArgumentParser(description='Takes in arguments from the command line')
@@ -77,7 +66,6 @@ _dry_run_only = parser.parse_args().dry_run
 _verbose_output = parser.parse_args().verbose
 #show_help_message = parser.parse_args().help
 
-
 #get a dictionary of files with extension as key and list of files as value
 def create_file_dictionary(file_list):
     """
@@ -91,43 +79,8 @@ def create_file_dictionary(file_list):
         dict: A dictionary where each key is a file extension category and the value is a list of file paths that belong to that category. Files with unknown file extensions are categorized as 'misc'.
     """
     
-    #TODO use Extensions_Dictionary instead of Extensions_categories
-
-    file_dictionary = {}
-    check_list = []
-    for file in file_list:
-        for dict in Extensions_categories:
-            for key, val in dict.items():
-                for ext in val:
-                    if file.endswith(f'.{ext}'):
-                        if key in file_dictionary:
-                            file_dictionary[key].append(file)
-                        else:
-                            file_dictionary[key] = [file]
-                        if file not in check_list:
-                            check_list.append(file)
-    #handle unknown file types as misc
-    for file in file_list:
-        if file not in check_list:
-            if 'misc' not in file_dictionary:
-                file_dictionary['misc'] = [file]
-            else:
-                file_dictionary['misc'].append(file)
-    return file_dictionary
-
-def create_file_dictionary2(file_list):
-    """
-    Creates a dictionary of files based on their file extension category.
-
-    Args:
-        file_list (list): A list of file paths.
-        extensions_category_list (list): A list of dictionaries where each dictionary contains a file extension category as the key and a list of file extensions as the value.
-
-    Returns:
-        dict: A dictionary where each key is a file extension category and the value is a list of file paths that belong to that category. Files with unknown file extensions are categorized as 'misc'.
-    """
-    
-    #TODO use Extensions_Dictionary instead of Extensions_categories
+    #TODO add contingency for files with no extensions
+    #     add contingency for empty file list
 
     file_dictionary = {}
     check_list = []
@@ -141,15 +94,16 @@ def create_file_dictionary2(file_list):
                         file_dictionary[key] = [file]
                     if file not in check_list:
                         check_list.append(file)
-    #handle unknown file types as misc
+    #handle unknown file types as unknown
     for file in file_list:
         if file not in check_list:
-            if 'misc' not in file_dictionary:
-                file_dictionary['misc'] = [file]
+            if 'unknowns' not in file_dictionary:
+                file_dictionary['unknowns'] = [file]
             else:
-                file_dictionary['misc'].append(file)
+                file_dictionary['unknowns'].append(file)
     return file_dictionary
 
+#quarantine suspicious files
 def run_security_checks(files_list):
     """
         Check each file for it's MIME type
@@ -185,17 +139,6 @@ def run_security_checks(files_list):
         print('Quarantined files have been deleted.')
     else:
         print('Quarantined files have not been deleted.')
-    
-def get_extensions():
-    #helper function for remove_duplicates_files
-    #Temporary, should be removed later.
-    #Unused
-    extensions = []
-    for f in Raw_File_List:
-        #if extensions doesn't contain the extension of f
-        if f.split('.')[-1] not in extensions:
-            extensions.append(f.split('.')[-1])
-    return extensions
 
 #find and return duplicate files using regex
 def get_duplicate_files(file_list):
@@ -230,14 +173,17 @@ def get_duplicate_files(file_list):
             if match:
                 duplicate_files.append(filename)
                 break
-    print(f'{len(duplicate_files)} duplicate files found...')
+    #print(f'{len(duplicate_files)} duplicate files found...')
     return duplicate_files
 
 #safely remove duplicate files. includes dry run functionality
 def remove_duplicates_files(files_list):    
-    #TODO Test real run
+    #TODO move dry_run to dry_run_mode
     #get list of duplicate files
     duplicates = get_duplicate_files(files_list)
+    if len(duplicates) == 0:
+        print('No duplicate files found.')
+        return
     #check if dry run
     if _dry_run_only:
         #print status
@@ -258,31 +204,98 @@ def remove_duplicates_files(files_list):
                 #print file being removed
                 print(f'\tRemoving {file.split("/")[-1]}...')
                 os.remove(file)
+    Raw_File_List = [f for f in get_absolute_file_paths(Target_Directory) if f not in get_app_made_zips()]
 
 #create folders for each key in file_dict
 def create_folders(file_dict):
-    print('creating folders...')
+    if _verbose_output:
+        print('creating folders...')
+    folders_created = []
     for key in file_dict.keys():
         # check if folder exists
         if not os.path.exists(f'{Target_Directory}/{key}'):
             # if not, create folder
-            print(f'\t{key}...')
+            if _verbose_output:
+                print(f'\t{key}...')
             os.mkdir(f'{Target_Directory}/{key}')
+            folders_created.append(key)
+    if _verbose_output:
+        for f in folders_created:
+            print(f'\t{f} folder created.')
 
 #create archives for each key in file_dict
 def create_archives(file_dict):
-    #TODO TEST!!! untested
     for key in file_dict.keys():
         # check if archive exists
         if not os.path.exists(f'{Target_Directory}/{key}.zip'):
             # if not, create archive
             shutil.make_archive(f'{Target_Directory}/{key}', 'zip', f'{Target_Directory}/{key}')
 
-# move files into folders, takes file_dict(category:filepath) as an argument
-def move_files(file_dict):
-    #TODO status messages for real run 
-    #TODO TEST!!! 
-    if _dry_run_only:
+#move or archive files, takes file_dict(category:filepath) as an argument
+def move_or_archive_files(file_dict):
+    #default mode is move
+
+    #set action for status message
+    action = ['Moving','moved']
+    to_zip = ''
+
+    if _archive_files:
+        action = ['Archiving','archived']
+        to_zip = '.zip'
+        create_archives(file_dict)
+    else:
+        create_folders(file_dict)
+        
+    print(f'{action[0]} {get_value_count_from_dictionary(file_dict)} files...')
+    
+    # iterate through file_dict
+    action_count = 0
+    for file_category, file_list in file_dict.items():
+        # iterate through files in value
+        print(f'{action[0]} {len(file_list)} file(s) to {file_category}{to_zip}...')
+        for file in file_list:
+            # check if file exists
+            if os.path.exists(f'{file}'):
+                # if so, move file to folder
+                print(f'\t{action[0]} {file}...')
+                
+                #archiving files
+                if _archive_files:
+                    if os.path.exists(f'{Target_Directory}/{file_category}.zip'):
+                        with zipfile.ZipFile(f'{Target_Directory}/{file_category}.zip', 'a') as zip:
+                            #remove absolutepath from filename before zipping
+                            zip.write(f'{file}', arcname=f'{file.split("/")[-1]}')
+                            #remove original file
+                            os.remove(f'{file}')
+                            action_count += 1
+                    else:
+                        #print error message
+                        print(f'\t{Target_Directory}/{file_category}.zip does not exist, skipping...')
+                
+                #Moving files
+                else:
+                    if os.path.exists(f'{Target_Directory}/{file_category}'):
+                        #move file
+                        #strip filepath from filename
+                        file_no_path = file.split('/')[-1]
+                        shutil.move(f'{file}', f'{Target_Directory}/{file_category}/{file_no_path}')
+                        action_count += 1
+                    else:
+                    #print error message
+                        print(f'\t{Target_Directory}/{file_category} does not exist, skipping...')
+    #print status
+    # ex: 5 files moved., 5 files archived.
+    print(f'{action_count} files {action[1]}.')
+
+#dry run. prints what would happen without actually changing anything
+def dry_run_mode(file_dict):
+    #TODO multimodal dry run
+
+    if _security_checks:
+        print('Dry run mode: security checks only.')
+    if _remove_duplicates:
+        print('Dry run mode: removing duplicates only.')
+    if _move_files:
         print('DRY RUN ONLY, NO FILES WILL BE MOVED.')
         #print which folders will be created
         #and which files go in which folders
@@ -293,96 +306,27 @@ def move_files(file_dict):
         print('Dry run complete, no files moved.')
         #stop function
         return
-    #print status
-    print(f'moving {get_value_count_from_dictionary(file_dict)} files...')
-    # create folders
-    create_folders(file_dict)
-    # iterate through file_dict
-    files_moved = 0
-    for file_category, file_list in file_dict.items():
-        # iterate through files in value
-        print(f'moving {len(file_list)} file(s) to {file_category}...')
-        for file in file_list:
-            # check if file exists
-            if os.path.exists(f'{file}'):
-                # if so, move file to folder
-                print(f'\tmoving {file}...')
-                #if target folder exists
-                if os.path.exists(f'{Target_Directory}/{file_category}'):
-                    #move file
-                    shutil.move(f'{file}', f'{Target_Directory}/{file_category}/{file}')
-                    files_moved += 1
-                else:
-                    #print error message
-                    print(f'\t{Target_Directory}/{file_category} does not exist, skipping...')
-    print(f'{files_moved} files moved.')
-
-#move files into archives, takes file_dict(category:filepath) as an argument
-def archive_files(file_dict):
-    #Test test test.  Completely untested
-    if _dry_run_only:
-        print('DRY RUN ONLY, NO FILES WILL BE ARCHIVED.')
-        #print which folders will be created
-        #and which files go in which folders
-        files_archived = 0
-        for key, value in file_dict.items():
-            print(f'zipping {len(value)} file(s) to {key}.zip...')
-            for file in value:
-                print(f'\t{file.split("/")[-1]}...')
-                files_archived += 1
-        print(f'Dry run complete, {files_archived} file(s) archived.')
-        #stop function
-        return
-    #print status
-    print(f'archiving {get_value_count_from_dictionary(file_dict)} file(s)...')
-    # create folders
-    create_archives(file_dict)
-    # iterate through file_dict
-    files_archived = 0
-    for file_category, file_list in file_dict.items():
-        # iterate through files in value
-        print(f'archiving {len(file_list)} file(s) to {file_category}.zip...')
-        for file in file_list:
-            # check if file exists
-            if os.path.exists(f'{file}'):
-                # if so, move file to folder
-                print(f'\tarchiving {file}...')
-                if os.path.exists(f'{Target_Directory}/{file_category}.zip'):
-                    with zipfile.ZipFile(f'{Target_Directory}/{file_category}.zip', 'a') as zip:
-                        #remove absolutepath from filename before zipping
-                        zip.write(f'{file}', arcname=f'{file.split("/")[-1]}')
-                        #remove original file
-                        os.remove(f'{file}')
-                        #zip.write(f'{file}')
-                    files_archived += 1
-                else:
-                    print(f'\t{Target_Directory}/{file_category}.zip does not exist, skipping...')
-    print(f'{files_archived} files archived.')
-
-
-
-
-def move_or_archive_files(file_dict):
-    #TODO combine move and archive functions here
-    
-    if _move_files:
-        move_files(file_dict)
-    elif _archive_files:
-        archive_files(file_dict)
-
-def dry_run_mode():
-    #TODO multimodal dry run
-
-    if _move_files:
-        print('Dry run mode: moving files only.')
     elif _archive_files:
         print('Dry run mode: archiving files only.')
-    if _remove_duplicates:
-        print('Dry run mode: removing duplicates only.')
 
+#debug info
+def print_debug_info():
+    #print which options are enabled
+        print(f'\n')
+        
+        statements = [("security_checks", _security_checks), 
+                      ("remove_duplicates", _remove_duplicates), 
+                      ("archive_files", _archive_files), 
+                      ("move_files", _move_files), 
+                      ("dry_run_only", _dry_run_only), 
+                      ("verbose", _verbose_output)]
 
-
-#troubleshooting functions
+        # Print the statements as two columns
+        for statement in statements:
+            print("{:<20} {}".format(statement[0], statement[1]))
+        print(f'\n')
+    
+#troubleshooting/debugging functions
 def get_value_count_from_dictionary(d):
     return sum(len(value) for value in d.values())
 def get_values_from_dict(d):
@@ -403,52 +347,48 @@ def find_dif_between_lists(list1, list2):
 def has_shared_value(dict_list):
     values = [v for d in dict_list for v in d.values()]
     return len(values) != len(set(map(str, values)))
-##########################################################################################################################
-##########################################################################################################################
-##########################################################################################################################
-##########################################################################################################################
 
+##########################################################################################################################
+##########################################################################################################################
+##########################################################################################################################
+##########################################################################################################################
 
 # main
 if __name__ == '__main__':
-   
-    print('Starting...\n\n')
-   
-    #print which options are enabled
-    print(f'security checks: {_security_checks}')
-    print(f'remove duplicates: {_remove_duplicates}')
-    print(f'archive files: {_archive_files}')
-    print(f'move files: {_move_files}')
-    print(f'dry run only: {_dry_run_only}')
-    print("\n\n.....................................................\n\n")
+    print('Starting...')
     
-    # prevent conflict between move and archive
-    if _move_files and _archive_files:
-        print('ERROR: Cannot move and archive files at the same time.')
+    if len(get_raw_file_list()) == 0:
+        print('No files found, exiting...')
         exit()
 
+    #debug output
+    if _verbose_output:
+      print_debug_info()
+    print(".....................................................\n\n")
     
-
-
     if _dry_run_only:
-        dry_run_mode()
+        dry_run_mode(create_file_dictionary(get_raw_file_list()))
         print("Done!")
         exit()
 
+    # Preliminary tasks.................
+
     # run security checks
     if _security_checks:
-        run_security_checks(Raw_File_List)
+        run_security_checks(get_raw_file_list())
     # run duplicates removal
     if _remove_duplicates:
-        remove_duplicates_files(Raw_File_List)
+        remove_duplicates_files(get_raw_file_list())
     
+    #Primary tasks......................
+
     if _move_files ^ _archive_files:
     # Either _move_files or _archive_files is True, but not both
-       move_or_archive_files(create_file_dictionary2(Raw_File_List))
+       move_or_archive_files(create_file_dictionary(get_raw_file_list()))
     elif _move_files and _archive_files:
     # Both _move_files and _archive_files are either True or False
         print('ERROR: Cannot move and archive files at the same time.')
+        print('exiting...')
         exit()
     
-
     print('Done!')
